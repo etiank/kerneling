f009e55bf314a0e1053f6ee74c053a30db6f4183
etiank <krizmanetian@gmail.com>
2023-12-29 23:43:03 +0100

    Added general structure of the GUI: changed frame size to fit everything (might try frame.pack() ?), added the buttons and selecting menus to their designated panels, added selected image previewer (not working, just for show).

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.text.Position;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.BrokenBarrierException;
@@ -34,36 +37,72 @@ public class GUI implements ActionListener {
 //      ───adding some silliness───────────────────────────────────────┐
         // le magestic, le one and only, J. Frame Kernely
         JFrame frame = new JFrame("Image process kerneling 🌽");
-        ImageIcon icon = new ImageIcon("cat.png");
+        ImageIcon icon = new ImageIcon("cat2.jpeg");
         frame.setIconImage(icon.getImage());
-        frame.setSize(400, 300);
+        frame.setSize(700, 300);
 //      ───────────────────────────────────────────────────────────────┘
 
 //      ─────────────────────────JPannelli──────────────────────────────────────────────────┐
-        JPanel panel1 = new JPanel(new BorderLayout());
-        JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
-        JPanel panel3 = new JPanel(new BorderLayout());
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JLabel p1 = new JLabel("Select kernel: ");
+        JLabel p2 = new JLabel("Select image: ");
+        JLabel p3 = new JLabel("Select run mode: ");
         //panel1.setSize(400, 300);
-        panel1.setBorder(createEmptyBorder(30, 30, 10, 30));
+        //panel1.setBorder(createEmptyBorder(30, 30, 10, 30));
 
         //panel.setLayout(new GridLayout(0,1 ));
         //panel.setBackground(Color.WHITE);
 //      ────────────────────────────────────────────────────────────────────────────────────┘
 
-//      ───────────────────🔘────────────────────────────────────┐
-        JLabel label = new JLabel("Number");
-        JButton button = new JButton("0");
-        button.addActionListener(this);
+//      ───────────────────🔘──────────────────────────────────────────────┐
+        JButton customKernel = new JButton("Select ");
+        JButton selectImage = new JButton("Select image");
+        JButton runButton = new JButton("Run");
+        // DROPDOWN MENU
+        JComboBox<String> runMode = new JComboBox<>(
+                new String[]{"Sequential", "Parallel", "Distributed"});
+        runMode.addActionListener((e) -> {
+            String selectedOption = (String) runMode.getSelectedItem();
+            System.out.println("Selected option: " + selectedOption);
+        });
 
-        JFileChooser jf = new JFileChooser("c:");
-        jf.showSaveDialog(null);
-//      ─────────────────────────────────────────────────────────┘
+        //  IMAGE PREVIEW
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File("cat2.jpeg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        JLabel imagePreview = new JLabel(new ImageIcon(image));
+
+        // BUTTON ACTIONLISTENERS
+        selectImage.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new java.awt.FileDialog((java.awt.Frame) null).setVisible(true);
+            }
+        });
+
+
+//      ───────────────────────────────────────────────────────────────────┘
 
 //      ───adding to panel───────────────────────────────────────┐
-        panel2.add(button, BorderLayout.SOUTH);
-        panel1.add(label);
-        frame.add(panel1);
-        frame.add(panel2);
+        panel1.add(p1, BorderLayout.NORTH);
+        panel1.add(customKernel, BorderLayout.CENTER);
+        panel2.add(p2, BorderLayout.NORTH);
+        panel2.add(selectImage, BorderLayout.CENTER);
+        panel2.add(imagePreview);
+        panel3.add(p3, BorderLayout.NORTH);
+        panel3.add(runMode, BorderLayout.WEST);
+        panel3.add(runButton, BorderLayout.EAST);
+
+
+        //frame.setLayout(new GridLayout(2,1));
+
+        frame.add(panel1, BorderLayout.WEST);
+        frame.add(panel2, BorderLayout.EAST);
+        frame.add(panel3, BorderLayout.SOUTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.pack();
         frame.setVisible(true);
